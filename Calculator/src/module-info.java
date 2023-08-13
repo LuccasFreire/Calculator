e0ee7cc3c57d239c06974cd9f7753acbef6b97e7
LuccasFreire <93498458+LuccasFreire@users.noreply.github.com>
2023-08-12 23:58:55 -0300

    Finished layout fxml

--- Calculator/src/module-info.java
+++ Calculator/src/module-info.java
@@ -1,6 +1,8 @@
 module Calculator {
 	requires javafx.controls;
 	requires javafx.base;
+	requires javafx.graphics;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
