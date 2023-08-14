714449e04664579ffc86a999fe69c68abe113888
LuccasFreire <93498458+LuccasFreire@users.noreply.github.com>
2023-08-13 23:07:19 -0300

    Finished calculate method finished processNumbers method

--- Calculator/src/application/MainController.java
+++ Calculator/src/application/MainController.java
@@ -1,11 +1,27 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class MainController {
-	public void processNumbers(ActionEvent event) {	
-	}
+	@FXML
+	private long n1 = 0;
+	private String operator = "";
+	private boolean start = true;
+	private Label result;
 	
+	@FXML
+	public void processNumbers(ActionEvent event) {
+		if (start = true) {
+			result.setText("");
+			start = false;
+		}
+		String value = ((Button)event.getSource()).getText();
+		result.setText(result.getText() + value);
+	}
+	@FXML
 	public void processOperators(ActionEvent event) {	
 	}
 }
--- Calculator/src/application/Model.java
+++ Calculator/src/application/Model.java
@@ -2,6 +2,20 @@ package application;
 
 public class Model {
 	public float calculate(long n1, long n2, String operator) {
-		return 0;
+		switch (operator) {
+		case "":
+			return n1 + n2;
+		case "-":
+			return n1 - n2;
+		case "*":
+			return n1 * n2;
+		case "/":
+			if (n2 == 0) {
+				return 0;
+			}
+			return n1 / n2;
+		default:
+			return 0;
+		}
 	}
 }
