50262b0dc1a72dda512ba1ceee5b547aeb90fd82
LuccasFreire <93498458+LuccasFreire@users.noreply.github.com>
2023-08-14 20:53:28 -0300

    Changed long to float

--- Calculator/src/application/MainController.java
+++ Calculator/src/application/MainController.java
@@ -9,38 +9,40 @@ public class MainController {
 	@FXML
 	private Label result;
 	private float n1 = 0;
+	private float n2 = 0;
 	private String operator = "";
 	private boolean start = true;
 	private Model model = new Model();
 	
 	@FXML
 	public void processNumbers(ActionEvent event) {
-		if (start) {
+		if(start) {
 			result.setText("");
 			start = false;
 		}
 		String value = ((Button)event.getSource()).getText();
-		result.setText(result.getText() + value);
+		result.setText(result.getText()+value);
 	}
+	
+
 	@FXML
 	public void processOperators(ActionEvent event) {
 		String value = ((Button)event.getSource()).getText();
-		
-		if (!value.equals("=")) {
-			if (!operator.isEmpty())
+		if(!value.equals("=")) {
+			if(!operator.isEmpty())
 				return;
 			
 			operator = value;
 			n1 = Float.parseFloat(result.getText());
 			result.setText("");
 		} else {
-			if (operator.isEmpty())
+			if(operator.isEmpty())
 				return;
 			
-			float n2 = Float.parseFloat(result.getText());
+			n2 = Float.parseFloat(result.getText());
 			float calculo = model.calculate(n1, n2, operator);
 			result.setText(Float.toString(calculo));
-			operator = "";
+			operator ="";
 			start = true;
 		}
 	}
--- Calculator/src/application/Model.java
+++ Calculator/src/application/Model.java
@@ -1,21 +1,23 @@
 package application;
 
 public class Model {
-	public float calculate(float n1, float n2, String operator) {
+
+	public float calculate(float number1, float number2, String operator) {
 		switch (operator) {
-		case "":
-			return n1 + n2;
+		case "+":
+			return number1+number2;
+			
 		case "-":
-			return n1 - n2;
-		case "*":
-			return n1 * n2;
+			return number1-number2;
 		case "/":
-			if(n2 == 0)
+			if(number2==0)
 				return 0;
-			return n1 / n2;
-		
-		default:
-			return 0;
+			return number1/number2;
+		case "*":
+			return number1*number2;
+
+		default:return 0;
 		}
 	}
-}
+
+}	
